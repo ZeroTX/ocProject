5776049372b86a36e9d2fc205d91604190cecb37
ZeroTX <1292788208@qq.com>
2018-07-04 17:33:03 +0800

    增加七牛云对象存储组件

---
+++ ocCommon/src/main/java/com/online/college/common/orm/LongModel.java
@@ -0,0 +1,21 @@
+package com.online.college.common.orm;
+
+import java.io.Serializable;
+
+/**
+ * Created by tx on 2018/7/4.
+ */
+public class LongModel implements Identifier<Long>,Serializable {
+    private static final long serialVersionUID = 7978917143723588623L;
+
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Long getId() {
+        return null;
+    }
+}
