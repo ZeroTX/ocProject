b0be53b5af6969b14bd486f66a148a1876cfdd19
ZeroTX <1292788208@qq.com>
2018-07-09 10:33:48 +0800

    添加工具类

--- ocCommon/src/main/java/com/online/college/common/storage/FileType.java
+++ ocCommon/src/main/java/com/online/college/common/storage/FileType.java
@@ -4,9 +4,11 @@ package com.online.college.common.storage;
  * Created by tx on 2018/7/3.
  */
 public enum FileType {
-    JPEG("E"),
-    PNG("S"),
-    GIF("2");
+    JPEG("FFD8FF"),
+
+    PNG("89504E47"),
+
+    GIF("47494638");
 
     private String value = "";
 
